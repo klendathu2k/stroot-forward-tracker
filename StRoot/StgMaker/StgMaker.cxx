e669ce9584c7fa9f8b2178c1017db7c5f815f324
Jason C. Webb <jwebb@bnl.gov>
2019-10-09 11:46:47 -0400

    Include the forward tracker header

--- StRoot/StgMaker/StgMaker.cxx
+++ StRoot/StgMaker/StgMaker.cxx
@@ -2,6 +2,10 @@
 #include <map>
 #include <string>
 
+#define LOGURU_IMPLEMENTATION 1
+#include "Tracker/FwdTracker.h"
+
+
 //________________________________________________________________________
 StgMaker::StgMaker() : StMaker("stg") {
 
@@ -9,10 +13,11 @@ StgMaker::StgMaker() : StMaker("stg") {
 //________________________________________________________________________
 int StgMaker::Init() {
 
+  // Initialize configuration file
   std::string configFile = "config.xml";
   std::map<string, string> cmdLineConfig;
   _xmlconfig.loadFile( configFile, cmdLineConfig );
-
+  // Dump configuration to screen
   LOG_INFO << _xmlconfig.dump().c_str() << endm;
 
   return kStOK;
