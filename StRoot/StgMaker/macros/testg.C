1b3a63f9e0dc9fa4cca228975740e6baa8bd9aed
Jason C. Webb <jwebb@bnl.gov>
2019-10-09 14:34:12 -0400

    load genfit

--- StRoot/StgMaker/macros/testg.C
+++ StRoot/StgMaker/macros/testg.C
@@ -2,6 +2,7 @@ void testg() {
 
   gROOT->LoadMacro("bfc.C");
   bfc(0,"nodefault");
+  gSystem->Load("../GenFit/lib/libgenfit2.so");
   gSystem->Load("StgUtil.so");
   gSystem->Load("StgMaker.so");
   
